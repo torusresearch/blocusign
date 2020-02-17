4b3cb5ddeb7d2b2d0a0f75cb6f40a8d446a0f05e
tetratorus <tetratorus@gmail.com>
2020-02-17 14:57:36 -0800

    use 443

--- express.js
+++ express.js
@@ -2,7 +2,7 @@ const express = require("express")
 const fs = require("fs")
 const https = require("https")
 const app = express()
-const port = 3000
+const port = 443
 
 app.use(express.static("dist"))
 
