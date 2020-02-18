f83cb6b84f6b4121b0da874f6a534bf8e13b6d9b
tetratorus <tetratorus@gmail.com>
2020-02-18 10:57:54 -0800

    Add signature component

--- src/components/Signature.vue
+++ src/components/Signature.vue
@@ -1,52 +1,86 @@
 <template>
   <v-container fluid>
     <v-row justify="center" align="center">
-      <v-col cols="2" justify="center" align="center">
-        <v-img v-if="image" :src="image"></v-img>
-        {{ verifier }} | {{ verifierid }} | {{ name }}
+      <v-col cols="3">
+        <v-row justify="center" align="center">
+          <div class="signature-container">
+          <v-col
+            class="no-margin-padding"
+            cols="12"
+            justify="center"
+            align="center"
+          >
+            <p id="signature-text">{{ name }}</p>
+          </v-col>
+          <v-col
+            class="no-margin-padding"
+            id="small-text"
+            cols="12"
+            justify="center"
+            align="center"
+          >
+            <v-row justify="center" align="center">
+                <logo :verifier="verifier" />
+                {{ verifierid }}
+            </v-row>
+          </v-col>
+          </div>
+        </v-row>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-const TextSignature = require("text-signature")
-const defaultOpts = {
-  width: 300,
-  height: 300,
-  paddingX: 100,
-  paddingY: 100,
-  canvasTargetDom: ".js-canvasTargetDom",
-  font: ["50px", "'Homemade Apple'"],
-  color: "blue",
-  textString: "Your Text HERE",
-  customFont: {
-    name: "'Homemade Apple'",
-    url: "https://fonts.googleapis.com/css?family=Homemade+Apple"
-  }
-}
-
+import Logo from "./Logo.vue"
 export default {
   data() {
     return {
       textSignature: null,
-      image: null,
+      image: null
     }
+  },
+  components: {
+    logo: Logo
   },
   props: {
     verifier: String,
     verifierid: String,
-    name: String,
+    name: String
   },
-  mounted() {
-    window.SSS = this
-    this.textSignature = new TextSignature({
-      ...defaultOpts,
-      ...{
-        textString: this.name
-      }
-    })
-    this.image = this.textSignature.getImageData()
-  }
 }
 </script>
+
+<style scoped>
+.signature-container {
+  padding: 5px 5px 5px 5px;
+  border: solid #1976d2 2px;
+  border-radius: 10px;
+  overflow: hidden;
+  min-width: 100px;
+}
+#signature-text {
+  font-family: "Homemade Apple";
+  margin-bottom: -10px;
+  padding: 5px 5px 5px 5px;
+  font-size: 20px;
+}
+#small-text {
+  font-size: 12px;
+}
+.no-margin-padding {
+  margin: 0 0 0 0;
+  padding: 0 0 0 0;
+}
+@font-face {
+  font-family: "Homemade Apple";
+  font-style: normal;
+  font-weight: 400;
+  src: local("Homemade Apple Regular"), local("HomemadeApple-Regular"),
+    url(https://fonts.gstatic.com/s/homemadeapple/v10/Qw3EZQFXECDrI2q789EKQZJob0x6XHgOiJM6.woff2)
+      format("woff2");
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA,
+    U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215,
+    U+FEFF, U+FFFD;
+}
+</style>
