c4a59b642c84e2cb6f439f1518383770405422b4
tetratorus <tetratorus@gmail.com>
2020-02-17 19:29:45 -0800

    Add multer

--- express.js
+++ express.js
@@ -1,16 +1,23 @@
-const express = require("express")
-const fs = require("fs")
-const https = require("https")
+const express = require('express')
+const multer = require('multer')
+const fs = require('fs')
+const https = require('https')
 const app = express()
 const port = 443
+const upload = multer({
+  dest: 'uploads/' // this saves your file into a directory called 'uploads'
+})
 
-app.use(express.static("dist"))
+app.use(express.static('dist'))
+app.post('/upload', upload.single('pdf-document'), (req, res) => {
+  res.redirect('/')
+})
 
 https
   .createServer(
     {
-      key: fs.readFileSync("./ssl/server.key"),
-      cert: fs.readFileSync("./ssl/server.crt")
+      key: fs.readFileSync('./ssl/server.key'),
+      cert: fs.readFileSync('./ssl/server.crt')
     },
     app
   )
